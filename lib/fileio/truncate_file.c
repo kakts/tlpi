bbd0d71d320fc9e59fa3a26a943d2a89d0b1dd0a
Keita Akutsu <kakts.git@gmail.com>
2017-10-15 23:50:53 +0900

    Fix fileio/truncate_file.c

--- lib/fileio/truncate_file.c
+++ lib/fileio/truncate_file.c
@@ -2,7 +2,10 @@
 #include <fcntl.h>
 #include "../tlpi_hdr.h"
 
+// Chapter5.8
 // truncateのサンプルコード
+// truncateは、open()またはそれ以外の方法で得たファイルディスクリプタを用いずに
+// ファイル内容を変更できるという珍しい特徴を持った唯一のシステムコール
 int main(int argc, char *argv[])
 {
   int result_truncate, size_length;
@@ -12,7 +15,7 @@ int main(int argc, char *argv[])
 
   printf("size_length: %d\n", size_length);
 
-  result_truncate = truncate("test.txt", size_length);
+  result_truncate = truncate(argv[1], size_length);
   if (result_truncate == -1) {
     printf("truncate file failed\n");
     errExit("truncate");
