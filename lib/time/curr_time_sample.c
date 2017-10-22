880da2ba041e29f0918edd10bc27fb17a035d5cf
Keita Akutsu <kakts.git@gmail.com>
2017-10-22 22:43:05 +0900

    Chapter10.2 Add current time sample

---
+++ lib/time/curr_time_sample.c
@@ -0,0 +1,15 @@
+#include "../tlpi_hdr.h"
+#include "curr_time.h"
+
+int main(int argc, char *argv[])
+{
+  char *test1, *test2;
+
+  test1 = currTime(NULL);
+  printf("test1: %s\n", test1);
+  // 曜日を表示
+  test2 = currTime("%a: %A");
+
+
+  printf("test2: %s\n", test2);
+}
