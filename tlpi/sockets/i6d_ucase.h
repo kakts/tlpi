e2bf8e34e74b6ab9fce0d10a4e136a189afa9f04
kakts <kakts.git@gmail.com>
2024-09-24 00:20:06 +0900

    59-3: add i6d_ucase_sv.c

---
+++ tlpi/sockets/i6d_ucase.h
@@ -0,0 +1,13 @@
+/**
+ * @file i6d_ucase.h
+ * 59-2 i6d_ucase_sv.cとi6d_ucase_cl.cで使用するヘッダファイル
+ */
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <ctype.h>
+#include "tlpi_hdr.h"
+
+#define BUF_SIZE 10
+
+#define PORT_NUM 50002
